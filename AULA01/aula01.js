e1e5700e43610e0aedd1b38e686ebbbfe7798a4c
marianadesouzaaguiar <mariana_desouza_2001@hotmail.com>
2021-08-14 17:50:56 -0300

    first commit luizacode

---
+++ AULA01/aula01.js
@@ -0,0 +1,61 @@
+var number1 = 45;
+var number2 = 23;
+
+var result = (number1 + number2)/2;
+console.log(`O resultado da media foi ${result}`);
+
+
+
+console.log("Eu sou maravilhosa!!!!!");
+
+var nome = "Celiane";
+
+console.log(`${nome} é maravilhosa!!!`);
+
+
+var peso = 48;
+var altura = 1.66;
+var imc = peso/ (altura * altura);
+console.log(`O resultado do seu IMC foi de ${imc}`)
+
+var first = 10;
+var second = 27;
+var result = first > second ? 'First é maior' : 'Second é maior';
+console.log(result);
+
+
+var nota1 = 7;
+var nota2 = 8;
+var media = (nota1 + nota2)/2
+
+var result = media >= 7 ? `Sua media final foi:${media}. Aprovado` : `Sua media final foi:${media}. Reprovado.`  
+
+console.log(result);
+
+var nome1 = "Mariana";
+var nome2 = "Mariana";
+
+var result = nome1 == nome2;
+console.log(result);
+
+
+var first = "1";
+var second = 1;
+
+var result = first == second ? "igual" : "diferente";
+
+console.log(`O número ${first} é ${result} ao número ${second}`);
+
+
+var bola, pipa;
+bola = true;
+pipa = false;
+
+var resultado = bola && pipa;
+console.log(resultado)
+
+var idade = 20;
+
+var check =  (idade >= 16 && idade <= 18) || (idade > 60)  ? 'Você vai pagar meia entrada' : "Você vai pagar inteira"
+
+console.log(check)
